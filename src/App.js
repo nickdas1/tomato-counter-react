2eeadcb27e3418a5578615c49eb17e533ede6d57
nickdas1 <75402464+nickdas1@users.noreply.github.com>
2022-05-21 18:02:23 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
+  const [selectedDay, setSelectedDay] = useState("M");
+  const [allTomatoes, setAllTomatoes] = useState({});
+
+  function calculateTomatoes(operation) {
+    const newAllTomatoes = { ...allTomatoes };
+    const currentDayTomatoes = newAllTomatoes[selectedDay];
+
+    if (operation === 'add') {
+      currentDayTomatoes ? newAllTomatoes[selectedDay] = `${newAllTomatoes[selectedDay]} 🍅` : newAllTomatoes[selectedDay] = "🍅";
+    }
+
+    if (operation === 'subtract' && currentDayTomatoes) {
+      newAllTomatoes[selectedDay] = currentDayTomatoes.slice(0, -2);
+    }
+
+    setAllTomatoes(newAllTomatoes);
+  }
+
+  const days = ["M", "T", "W", "Th", "F", "Sa", "Su"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="window">
+        <h2>Tomato Counter</h2>
+        {days.map((day) => (
+          <div
+            key={day}
+            className="tomato-box"
+            onClick={() => setSelectedDay(day)}
+          >
+            <h3 className={selectedDay === day ? "selected-day" : ""}>{day}</h3>
+            <div className="tomato-day-box">{allTomatoes[day] ?? ""}</div>
+          </div>
+        ))}
+        <div className="buttons-container">
+          <div className="button" onClick={() => calculateTomatoes('subtract')}>
+            -
+          </div>
+          <div className="button" onClick={() => calculateTomatoes('add')}>
+            +
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
